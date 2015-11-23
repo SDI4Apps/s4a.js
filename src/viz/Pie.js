17d3617fa4c5733eb32f82c84813b41478774d91
Eirik Csák Knutsen <eck@avinet.no>
2015-11-23 11:19:21 +0100

    Refactoring, QA-fixes

--- src/viz/Pie.js
+++ src/viz/Pie.js
@@ -5,22 +5,87 @@ s4a.viz.pie = (function() {
      * @param {s4a.map} map object
      */
     function createChart(mDiagramData, map) {
-        var resize = function(feature) {
+
+        var getPosition = function (feature, featurePadding) {
+            var center = projectPoint(
+                feature.location.x,
+                feature.location.y
+            );
+
+            return [center[0] - featurePadding, center[1] - featurePadding];
+        };
+
+         var resize = function(feature) {
             var svg = feature.svg,
                 g = svg.selectAll('g'),
-                center = projectPoint(
-                    feature.location.x,
-                    feature.location.y
-                ),
                 newWidth = mDiagramData.width * (svg.attr('scale') || scale),
                 newWidthHalf = (newWidth / 2);
 
+            var center = getPosition(feature, newWidthHalf);
+
             svg.attr('width', newWidth)
                 .attr('height', newWidth)
-                .style('left', center[0] - newWidthHalf + 'px')
-                .style('top', center[1] - newWidthHalf + 'px');
+                .style('left', center[0] + 'px')
+                .style('top', center[1] + 'px');
 
             g.attr('transform', 'translate(' + newWidthHalf + ',' + newWidthHalf + ')');
+        };
+
+        var onClick = function () {
+            var currentScale = parseInt(svg.attr('scale'), 10) || scale,
+                newScale = currentScale === 1 ? scale : 1;
+
+            svg.selectAll('path')
+                .transition()
+                .duration(200)
+                .attrTween('transform', function(d, i, a) {
+                    return d3.interpolateString(a, 'scale(' + newScale + ')');
+                })
+                .attr('d', newScale === 1 ? bigArc : arc);
+
+            if (newScale === 1) {
+                svg.attr('scale', newScale)
+                // show selected pies at top
+                .style('z-index', 100);
+
+                var legendRectSize = 18;
+                var legendSpacing = 4;
+
+                g.append('rect')
+                    .attr('class', 'legend')
+                    .attr('x', -50)
+                    .attr('y', function(d, i) {
+                        return -40 + i * 20;
+                    })
+                    .attr('width', 15)
+                    .attr('height', 10)
+                    .style('fill', function(d) {
+                        return color(d.data._label);
+                    });
+
+                g.append('text')
+                    .attr('class', 'legend')
+                    .attr('x', -20)
+                    .attr('y', function(d, i) {
+                        return -40 + i * 20;
+                    })
+                    .text(function(d) {
+                        return d.data._label;
+                    });
+            } else {
+                g.selectAll('text').remove();
+                g.selectAll('rect').remove();
+
+                // show unselected pies at bottom
+                svg.style('z-index', 1);
+
+                // Wait until resize animation is complete before 
+                // applying new scale
+                setTimeout(function() {
+                    svg.attr('scale', newScale);
+                    resize(feature);
+                }, 200);
+            }
         };
 
         // Reposition the SVG to cover the features.
@@ -101,66 +166,13 @@ s4a.viz.pie = (function() {
 
         features.push(feature);
 
-        g.on('click', function() {
-            var currentScale = parseInt(svg.attr('scale'), 10) || scale,
-                newScale = currentScale === 1 ? scale : 1;
-
-            svg.selectAll('path')
-                .transition()
-                .duration(200)
-                .attrTween('transform', function(d, i, a) {
-                    return d3.interpolateString(a, 'scale(' + newScale + ')');
-                })
-                .attr('d', newScale === 1 ? bigArc : arc);
-
-            if (newScale === 1) {
-                svg.attr('scale', newScale)
-                // show selected pies at top
-                .style('z-index', 100);
-
-                var legendRectSize = 18;
-                var legendSpacing = 4;
-
-                g.append('rect')
-                    .attr('class', 'legend')
-                    .attr('x', -50)
-                    .attr('y', function(d, i) {
-                        return -40 + i * 20;
-                    })
-                    .attr('width', 15)
-                    .attr('height', 10)
-                    .style('fill', function(d) {
-                        return color(d.data._label);
-                    });
-
-                g.append('text')
-                    .attr('class', 'legend')
-                    .attr('x', -20)
-                    .attr('y', function(d, i) {
-                        return -40 + i * 20;
-                    })
-                    .text(function(d) {
-                        return d.data._label;
-                    });
-            } else {
-                g.selectAll('text').remove();
-                g.selectAll('rect').remove();
-
-                // show unselected pies at bottom
-                svg.style('z-index', 1);
-
-                // Wait until resize animation is complete before 
-                // applying new scale
-                setTimeout(function() {
-                    svg.attr('scale', newScale);
-                    resize(feature);
-                }, 200);
-            }
-        });
+        g.on('click', onClick);
 
         olMap.on('postcompose', function(event) {
             reset(features);
         });
+
+        
     }
 
     return {
